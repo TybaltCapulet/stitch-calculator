2d5264437ee8c56d2c7d30204b28ae8c615075dd
HenryPook <115218746+HenryPook@users.noreply.github.com>
2023-08-12 20:29:46 +0100

    Changes to Courses and About page

--- AboutPage.js
+++ AboutPage.js
@@ -11,9 +11,9 @@ const AboutPage = () => {
                 <View>
                     <Image style={styles.logo} source={require("./assets/secretslogo.png")} />
                     <Text style={styles.title}>About this app</Text>  
-                    <Text>This app was created to help people calculate the necessary length of thread when saddle stitching, instead of relying on guesswork.{'\n'}</Text>
+                    <Text>This app was created to help you calculate the necessary length of thread when saddle stitching, instead of relying on guesswork.{'\n'}</Text>
                     <Text>Nigel Armitage provided the original calculator on his website and sponsored the building of this app by Henry Rackley.{'\n'}</Text>
-                    <Text>With thanks to Graham of White Star Leather who wrote the original version of the calculator in Jquery on secretsfromtheworkshop.com{'\n'}</Text>
+                    <Text>With thanks to Graham of White Star Leather who wrote the original version of the calculator for www.secretsfromtheworkshop.com{'\n'}</Text>
                     <Text>Please contact Armitage Leather if you have any feedback or suggestions on how to improve the app via nigel@armitageleather.com{'\n'}</Text>
                     <Footer />
                 </View>
